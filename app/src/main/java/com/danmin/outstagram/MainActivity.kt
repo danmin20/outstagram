8310c125bdd26a3706dcbc6636c95b442fc28f5c
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-26 00:05:29 +0900

    implement upload photo

--- app/src/main/java/com/danmin/outstagram/MainActivity.kt
+++ app/src/main/java/com/danmin/outstagram/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.danmin.outstagram
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
-import com.danmin.outstagram.navigation.AlarmFragment
-import com.danmin.outstagram.navigation.DetailViewFragment
-import com.danmin.outstagram.navigation.GridFragment
-import com.danmin.outstagram.navigation.UserFragment
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.danmin.outstagram.navigation.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -26,6 +28,13 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
                 return true
             }
             R.id.action_add_photo -> {
+                if (ContextCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.READ_EXTERNAL_STORAGE
+                    ) == PackageManager.PERMISSION_GRANTED
+                ) {
+                    startActivity(Intent(this, AddPhotoActivity::class.java))
+                }
                 return true
             }
             R.id.action_favorite_alarm -> {
@@ -48,5 +57,10 @@ class MainActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemS
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         bottom_navigation.setOnNavigationItemSelectedListener(this)
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
     }
 }
